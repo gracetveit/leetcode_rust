c92ad8290f5bb5be6b4c149dcb45aec408d49c8e
Grace Tveit <grace.tveit@gmail.com>
2022-07-24 13:55:05 -0700

    feat: Adding conversion methods for ListNode

--- src/submissions/add_two_numbers.rs
+++ src/submissions/add_two_numbers.rs
@@ -9,8 +9,34 @@ pub struct ListNode {
 
 impl ListNode {
     #[inline]
-    fn new(val: i32) -> Self {
+    pub fn new(val: i32) -> Self {
         ListNode { next: None, val }
+    }
+
+    pub fn to_vec(&self) -> Vec<i32> {
+        match &self.next {
+            Some(x) => {
+                return vec![vec![self.val], x.to_vec()]
+                    .into_iter()
+                    .flatten()
+                    .collect()
+            }
+            None => return vec![self.val],
+        }
+    }
+
+    pub fn from_vec(data: Vec<i32>) -> Self {
+        match data.len() {
+            0 => panic!(),
+            1 => ListNode {
+                val: data[0],
+                next: None,
+            },
+            _ => ListNode {
+                val: data[0],
+                next: Some(Box::new(ListNode::from_vec(Vec::from(&data[1..])))),
+            },
+        }
     }
 }
 impl Solution {
@@ -19,4 +45,12 @@ impl Solution {
         l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
     }
+}
+
+#[test]
+fn list_node_conversion() {
+    let test_data = vec![0, 1, 2, 3, 4];
+    let test_list = ListNode::from_vec(test_data);
+
+    assert_eq!(test_list.to_vec(), [0, 1, 2, 3, 4])
 }
