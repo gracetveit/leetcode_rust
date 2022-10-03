eb37baf23b53553fbb84f2bb249dc61fadd7922d
Grace Tveit <grace.tveit@gmail.com>
2022-10-03 16:40:36 -0700

    feat: generate_n_digit_integers working

--- src/submissions/numbers_with_same_consecutive_diff.rs
+++ src/submissions/numbers_with_same_consecutive_diff.rs
@@ -6,23 +6,43 @@ impl Solution {
     /// Returns an arary of all integers of length `n` where the difference
     /// between every two consecutive digits is `k`.
     pub fn nums_same_consec_diff(n: i32, k: i32) -> Vec<i32> {
-        let mut n_digit_integers = Solution::generate_n_digit_integers(n);
+        let n_digit_integers = Solution::generate_n_digit_integers(n);
         n_digit_integers
             .into_iter()
             .filter(|x| Solution::consecutive_digits_is_k_long(x, k))
             .collect()
-        // todo!()
     }
 
     fn generate_n_digit_integers(n: i32) -> Vec<i32> {
-        todo!()
+        let mut digits = Vec::new();
+        for _ in 0..n {
+            digits.push(String::from("0"));
+        }
+        digits[0] = String::from("1");
+        let floor = digits.join("").parse::<i32>();
+
+        for i in 0..digits.len() {
+            digits[i] = String::from("9");
+        }
+        let top = digits.join("").parse::<i32>();
+
+        match (floor, top) {
+            (Ok(floor), Ok(top)) => {
+                let mut int_vec = Vec::new();
+                for i in floor..top + 1 {
+                    int_vec.push(i)
+                }
+                int_vec
+            }
+            (_, _) => panic!(),
+        }
+
+        // todo!()
     }
 
     fn consecutive_digits_is_k_long(n: &i32, k: i32) -> bool {
         todo!()
     }
-
-
 }
 
 #[test]
@@ -37,4 +57,11 @@ fn example_1() {
         10, 12, 21, 23, 32, 34, 43, 45, 54, 56, 65, 67, 76, 78, 87, 89, 98,
     ];
     assert_eq!(Solution::nums_same_consec_diff(2, 1), ans)
+}
+
+#[test]
+fn test_generate_n_digit_integers() {
+    let ans = Solution::generate_n_digit_integers(2);
+    assert_eq!(ans[0], 10);
+    assert_eq!(ans[ans.len() - 1], 99);
 }
